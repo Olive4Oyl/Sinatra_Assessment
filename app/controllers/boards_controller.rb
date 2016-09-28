8186d0958f12c2325976c2f9763cf38e5fc99e2b
Kelene Chung <simoy93@gmail.com>
2016-09-28 16:42:57 -0400

    added the boards controller file

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,58 @@
+require 'pry'
+class BoardsController < UsersController
+
+	get '/boards' do
+		@boards = Board.all
+		erb :'/boards/index'
+	end 
+
+	get '/boards/new' do 
+		if !logged_in?
+			redirect to "/login"
+		end
+		erb :'/boards/new'
+	end
+
+	post '/boards/new' do 
+		@user = User.find_by(session[:id])
+		@board = Board.create(params[:board])
+		@board.user_id = @user.id
+		@board.save
+		redirect to "/boards/#{@board.id}"
+	end
+
+	get '/boards/:id' do 
+		@board = Board.find_by(id: params[:id])
+		@user = current_user
+		@board.user_id = @user.id
+		@board.save
+		erb :'/boards/show'
+	end
+
+	get '/boards/:id/edit' do 
+		@user = current_user
+		if logged_in?
+			@board = Board.find_by(id: params[:id])
+			erb :'/boards/edit'
+		else
+			redirect to "/login"
+		end
+	end
+
+	post '/boards/:id/edit' do 
+			@board = Board.find_by(params[:id])
+			@board.update(params[:board])
+			@board.save
+			redirect to "/boards/#{@board.id}"
+	end
+
+
+
+	# get '/boards/:slug' do 
+	# 	@user = Board.find_by_slug(params[:slug])
+
+	# end
+
+
+
+end
